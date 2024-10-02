69f446646d86481b4cc00ee6391c9407cd6bfd76
vinutna.k <vinutna@gmail.com>
2024-10-02 13:41:24 +0530

    commit-1

---
+++ One.java
@@ -0,0 +1,13 @@
+public class One
+{   int a,b;
+    One(int a,int b)
+    {   this.a=a;
+        this.b=b;
+    }
+    void display()
+    {
+        System.out.println("Instance method of One class");
+        System.out.println("a:\t"+a);
+        System.out.println("b:\t"+b);
+    }
+}
